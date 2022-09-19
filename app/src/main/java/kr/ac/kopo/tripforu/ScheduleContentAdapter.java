f3716d87520e8adf9b0ac4ffd52bed049b8c658f
JeongDabin <ekqls92555@naver.com>
2022-09-20 01:45:13 +0900

    정다빈 1 여행일정 체크박스 숨기기 보이기 추가 2 체크박스 클릭 이벤트 내용 추가

--- app/src/main/java/kr/ac/kopo/tripforu/ScheduleContentAdapter.java
+++ app/src/main/java/kr/ac/kopo/tripforu/ScheduleContentAdapter.java
@@ -5,22 +5,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ScheduleContentAdapter extends RecyclerView.Adapter<ScheduleContentAdapter.ItemViewHolder>
-implements ItemTouchHelperListener{
+implements ItemTouchHelperListener, PageAdepter{
     private Context mContext;
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView textView;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            textView = itemView.findViewById(R.id.FRAMELAYOUT_RecyclerView);
-        }
-    }
 
     @NonNull
     @Override
@@ -28,22 +23,49 @@ implements ItemTouchHelperListener{
         mContext = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.layout_recycleviewschedule, parent, false);
-        View scheduleListView = inflater.inflate(R.layout.activity_schedulelist, parent, false);
         ScheduleContentAdapter.ItemViewHolder viewHolder = new ScheduleContentAdapter.ItemViewHolder(view);
-
+        CheckBox chb_AllScheduleList = ((LinearLayout)parent.getParent().getParent().getParent()).findViewById(R.id.CHB_AllScheduleList);
+        CheckBox chb_ScheduleList = view.findViewById(R.id.CHB_ScheduleList);
         view.setOnLongClickListener(new View.OnLongClickListener() {
-            int position = viewHolder.getAdapterPosition();
-            LinearLayout chb_ScheduleList = view.findViewById(R.id.CHB_ScheduleList);
-            LinearLayout chb_AllScheduleList = scheduleListView.findViewById(R.id.CHB_AllScheduleList);
-            LinearLayout btn_ScheduleDelete = scheduleListView.findViewById(R.id.BTN_ScheduleDelete);
             @Override
             public boolean onLongClick(View view) {
+                int position = viewHolder.getAdapterPosition();
+                TextView text_AllScheduleList = ((LinearLayout)parent.getParent().getParent().getParent()).findViewById(R.id.TEXT_AllScheduleList);
+                for (Schedule schedule:ScheduleController.remainingSchedule) {
+                    View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                    v.findViewById(R.id.CHB_ScheduleList).setVisibility(View.VISIBLE);
+                    v.findViewById(R.id.LAYOUT_ScrollHandle).setVisibility(View.VISIBLE);
+                }
+                LinearLayout layout_Scroll = view.findViewById(R.id.LAYOUT_ScrollHandle);
                 chb_AllScheduleList.setVisibility(View.VISIBLE);
-                Log.d("chb_AllScheduleList", chb_AllScheduleList+"");
-                Log.d("chb_ScheduleList", chb_ScheduleList+"");
-                chb_ScheduleList.setVisibility(View.VISIBLE);
-                btn_ScheduleDelete.setVisibility(View.VISIBLE);
-                btn_ScheduleDelete.bringToFront();
+                text_AllScheduleList.setVisibility(View.VISIBLE);
+                layout_Scroll.setVisibility(View.VISIBLE);
+
+                View v1 = pageAdepter.SetAppBarAction(2, false, "삭제");
+                View v2 = pageAdepter.SetAppBarAction(0, true, "취소");
+                v1.setOnClickListener(v3 -> {
+                    Toast.makeText(mContext, "삭제 버튼 클릭", Toast.LENGTH_SHORT).show();
+                    for (Schedule schedule:ScheduleController.remainingSchedule) {
+                        View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                        CheckBox check = v.findViewById(R.id.CHB_ScheduleList);
+                        /*if (check.isChecked() == true){
+                            ScheduleController.remainingSchedule.remove(schedule.GetId());
+                        }*/
+                    }
+                });
+                v2.setOnClickListener(v4 -> {
+                    Toast.makeText(mContext, "취소 버튼 클릭", Toast.LENGTH_SHORT).show();
+                    pageAdepter.ResetAppBar();
+                    chb_AllScheduleList.setVisibility(View.GONE);
+                    layout_Scroll.setVisibility(View.GONE);
+                    text_AllScheduleList.setVisibility(View.GONE);
+                    for (Schedule schedule:ScheduleController.remainingSchedule) {
+                        View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                        v.findViewById(R.id.CHB_ScheduleList).setVisibility(View.GONE);
+                        v.findViewById(R.id.LAYOUT_ScrollHandle).setVisibility(View.GONE);
+                    }
+
+                });
                 return false;
             }
         });
@@ -52,13 +74,50 @@ implements ItemTouchHelperListener{
             @Override
             public void onClick(View view) {
                 String data = "";
-
                 int position = viewHolder.getAdapterPosition();
                 if (position != RecyclerView.NO_POSITION) {
-
                 }
-                Log.d("position", position+"");
-                Log.d(data, "data: ");
+            }
+        });
+
+        chb_ScheduleList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckBox check;
+                check = view.findViewById(R.id.CHB_ScheduleList);
+                if (chb_AllScheduleList.isChecked() == true){
+                    chb_AllScheduleList.setChecked(false);
+                }else {
+                    for (Schedule schedule:ScheduleController.remainingSchedule) {
+                        View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                        check = v.findViewById(R.id.CHB_ScheduleList);
+                        if (check.isChecked() == false){
+                            break;
+                        }
+                    }
+                    if (check.isChecked() == true){
+                        chb_AllScheduleList.setChecked(true);
+                    }
+                }
+            }
+        });
+
+        chb_AllScheduleList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (chb_AllScheduleList.isChecked() == true){
+                    for (Schedule schedule:ScheduleController.remainingSchedule) {
+                        View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                        CheckBox check = v.findViewById(R.id.CHB_ScheduleList);
+                        check.setChecked(true);
+                    }
+                }else {
+                    for (Schedule schedule:ScheduleController.remainingSchedule) {
+                        View v = parent.findViewWithTag("schItem: "+schedule.GetId());
+                        CheckBox check = v.findViewById(R.id.CHB_ScheduleList);
+                        check.setChecked(false);
+                    }
+                }
             }
         });
         return viewHolder;
@@ -92,17 +151,16 @@ implements ItemTouchHelperListener{
         ScheduleController.remainingSchedule.remove(position);
         notifyItemRemoved(position);
     }
-    public void onClick(View view){
 
-    }
     public class ItemViewHolder extends RecyclerView.ViewHolder {
+        View itemHolder;
         TextView name;
         TextView destination;
         TextView grade;
         TextView startDate;
         TextView likes;
         TextView sharedCount;
-        LinearLayout layout_ScheduleText , layout_SharedContents, layout_SharedMark;
+        LinearLayout layout_ScheduleText , layout_SharedContents;
         boolean isShared;
         private int number;
 
@@ -116,12 +174,10 @@ implements ItemTouchHelperListener{
             likes = itemView.findViewById(R.id.TEXT_Likes);
             sharedCount = itemView.findViewById(R.id.TEXT_SharedCount);
             layout_SharedContents = itemView.findViewById(R.id.LAYOUT_SharedContents);
-            layout_SharedMark = itemView.findViewById(R.id.LAYOUT_SharedMark);
-
+            itemHolder = itemView;
         }
 
         public void onBind(Schedule item, int position) {
-            if (item.CheckIsShared() == true){
                 name.setText(item.GetName());
                 destination.setText(item.GetDestination());
                 grade.setText(Math.floor(item.GetRatingBar() * 10) / 10 + "");
@@ -129,19 +185,7 @@ implements ItemTouchHelperListener{
                 likes.setText(item.GetLikes() + "");
                 sharedCount.setText(item.GetSharedCount() + "");
                 item.SetNumber(position);
-            }else{
-                name.setText(item.GetName());
-                destination.setText(item.GetDestination());
-                grade.setText(Math.floor(item.GetRatingBar() * 10) / 10 + "");
-                startDate.setText(item.GetStartDate());
-                likes.setText(item.GetLikes() + "");
-                sharedCount.setText(item.GetSharedCount() + "");
-                item.SetNumber(position);
-                layout_SharedMark.setVisibility(View.GONE);
-                layout_SharedContents.setVisibility(View.GONE);
-            }
-
-
+                itemHolder.setTag("schItem: "+item.GetId());
         }
     }
 }
