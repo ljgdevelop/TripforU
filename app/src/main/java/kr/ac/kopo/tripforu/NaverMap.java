fa098279323be6982366ec3f6cba4ca7a87b02a6
kim <rlqor3398@gmail.com>
2022-09-21 21:12:53 +0900

    김기백
    네이버 지도 xml, 자바 추가
    네이버 지도 호환 되게 빌드, 세팅 gradle 추가설정
    매니피스트 meta-data 네이버 쿨라이언트 ID 및 암호 추가

---
+++ app/src/main/java/kr/ac/kopo/tripforu/NaverMap.java
@@ -0,0 +1,37 @@
+package kr.ac.kopo.tripforu;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.OnMapReadyCallback;
+
+public class NaverMap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private static NaverMap naverMap;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.layout_naver_map);
+
+        NaverMapSdk.getInstance(this).setClient(
+            new NaverMapSdk.NaverCloudPlatformClient("3l42j6ptcl")
+        );
+
+        mapView = (MapView)findViewById(R.id.map_View);
+        mapView.onCreate(saveInstanceState);
+        mapView.getMapAsync(this);
+
+
+    }
+    @Override
+    public void onMapReady(@NonNull com.naver.maps.map.NaverMap naverMap) {
+
+    }
+}
