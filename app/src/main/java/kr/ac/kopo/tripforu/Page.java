bc0df7fde578b6a6e06db44ea5bf4fa4140b63d0
leejegyeong <lkiwi5001@gmail.com>
2022-09-27 16:53:10 +0900

    이제경 생성자의 첫번째 매개변수 변경 Object -> View

--- app/src/main/java/kr/ac/kopo/tripforu/Page.java
+++ app/src/main/java/kr/ac/kopo/tripforu/Page.java
@@ -1,16 +1,17 @@
 package kr.ac.kopo.tripforu;
 
 import android.app.Activity;
+import android.view.View;
 
 class Page{
     private Object pageObject = null;
     private byte type = 0;
     
-    public Page(Object page, byte type){
+    public Page(View page, byte type){
         this.pageObject = page;
         this.type = (byte) type;
     }
-    public Page(Activity page, byte type){
+    public Page(Activity page,byte type){
         this.pageObject = (Object) page;
         this.type = (byte) type;
     }
