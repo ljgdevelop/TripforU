8b0a983ff8d1f657f033dd6478bb424ba5542aea
leejegyeong <lkiwi5001@gmail.com>
2022-09-28 00:36:24 +0900

    이제경 미사용/불필요 파일 삭제

--- app/src/main/java/kr/ac/kopo/tripforu/ItemTouchHelperListener.java
+++
@@ -1,6 +0,0 @@
-package kr.ac.kopo.tripforu;
-
-public interface ItemTouchHelperListener {
-    boolean onItemMove(int from_Position, int to_Position);
-    void onItemSwipe(int Position);
-}
--- app/src/main/java/kr/ac/kopo/tripforu/SlideButton.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.tripforu;
-
-import android.os.Bundle;
-
-class SlideButton extends ActivitySettings{
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-    }
-}
