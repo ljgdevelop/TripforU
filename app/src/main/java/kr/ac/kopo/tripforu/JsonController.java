a875a9e8f547c0714c0493ad62ccf5adf318af52
leejegyeong <lkiwi5001@gmail.com>
2022-10-03 23:29:31 +0900

    이제경 불필요 import 제거

--- app/src/main/java/kr/ac/kopo/tripforu/JsonController.java
+++ app/src/main/java/kr/ac/kopo/tripforu/JsonController.java
@@ -2,28 +2,18 @@ package kr.ac.kopo.tripforu;
 
 
 import android.content.Context;
-import android.content.res.AssetManager;
-import android.os.Environment;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.json.simple.JSONArray;
