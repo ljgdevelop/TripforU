0ca14c32254face47741b20b5502383264112f43
leejegyeong <lkiwi5001@gmail.com>
2022-10-03 23:32:46 +0900

    이제경 추천 일정 layout 추가

---
+++ app/src/main/java/kr/ac/kopo/tripforu/ActivityRecommend.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.tripforu;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.File;
+import java.time.LocalDate;
+
+public class ActivityRecommend extends PageController {
+    @Override protected boolean useToolbar(){ return true; }
+    
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend);
+        
+        /*LayoutRecommendBanner banner = new LayoutRecommendBanner(getApplicationContext());
+        ((ViewGroup)findViewById(R.id.LAYOUT_Recommend_Container)).addView(banner);
+    
+        LayoutRecommendBanner smallbanner = new LayoutRecommendBanner(getApplicationContext(), 1);
+        ((ViewGroup)findViewById(R.id.LAYOUT_Recommend_Container)).addView(smallbanner);*/
+    }
+}
